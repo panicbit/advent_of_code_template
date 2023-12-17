225e5775c67d1db6b7ef10d81d19cb6017c1ab14
panicbit <panicbit.dev@gmail.com>
2023-12-17 03:11:02 +0100

    day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,18 @@
 use aoc::aoc;
 
 #[aoc(2023, 1, 1)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let digits = line
+                .chars()
+                .filter(|c| c.is_ascii_digit())
+                .map(|c| c as u32 - '0' as u32)
+                .collect::<Vec<_>>();
+            let number = 10 * digits[0] + digits[digits.len() - 1];
+
+            number
+        })
+        .sum::<u32>()
 }
